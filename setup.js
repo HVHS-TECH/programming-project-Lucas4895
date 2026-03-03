fed719bf939dc3cb3313ca461041ed8a04a3f9a5
Lucas4895 <23554ln@hvhs.school.nz>
2026-03-03 15:12:16 +1300

    3 March 2026 added player's movements

--- setup.js
+++ setup.js
@@ -17,11 +17,13 @@ let gameState = 'start';
 function setup(){
     cnv = new Canvas(900,900)
 
-    
-    startButton = new Sprite(100,100,100,100);
+    startButton = new Sprite(width/2,height/2,250,100);
     startButton.color = "#84b3b0";
-    continueButton = new Sprite(200,200,100,100);
+    continueButton = new Sprite(width/2,height/2,100,100);
+    continueButton.visible = false
 
+    player = new Sprite(width/2, 850, 50);
+    player.visible = false
 }
 
 
@@ -31,21 +33,27 @@ function setup(){
 //
 /*******************************************************/
 function drawStartScreen() {
-    if (mouseIsPressed === true) {
-        gameState = 'instruction';     
+    if (startButton.onMousePressed === true) {
+        gameState = 'instruction';  
+        startButton.visible = false;
+        startButton.collides = false;
     } 
 };
 
 
 /*******************************************************/
-//drawStartScreen()
+//drawInstructionScreen()
 //
 //
 /*******************************************************/
 function drawInstructionScreen() {
+ 
+    continueButton.visible = true;
+    
     if (mouseIsPressed === true) {
         gameState = 'gameplay';     
-    }     
+
+    }
 }
 
 
@@ -55,21 +63,54 @@ function drawInstructionScreen() {
 //
 /*******************************************************/
 function drawGameplayScreen() {
-    startButton.visible = "false";
+     continueButton.visible = false;
+    player.visible = true;
+    
+
+/*******************************************************/
+//player's movements
+//
+/*******************************************************/
+    if (kb.pressing('A')) {
+        player.vel.x = -5;
+    } else if (kb.pressing('D')){
+        player.vel.x = +5;
+    }
+        if (kb.released('A')) {
+        player.vel.x = 0;
+    } else if (kb.released('D')){
+        player.vel.x = 0;
+    }
+        if (kb.pressing('W')) {
+        player.vel.y = -5;
+    } else if (kb.pressing('S')){
+        player.vel.y = +5;
+    }
+        if (kb.released('W')) {
+        player.vel.y = 0;
+    } else if (kb.released('S')){
+        player.vel.y = 0;
+    }
 }
 
 /*******************************************************/
 //draw()
 //if gameState is 'start', call drawStartScreen()
-//else call drawGameplayScreen()
+//
 //
 //
 /*******************************************************/
 function draw() {
-    background('#000');
+    background('#000')
     if (gameState === 'start') {
         drawStartScreen();
-    } else {
+    }
+
+    if (gameState === 'instruction') {
+        drawInstructionScreen();
+    }
+
+    if (gameState === 'gameplay') {
         drawGameplayScreen();
     }
 }
